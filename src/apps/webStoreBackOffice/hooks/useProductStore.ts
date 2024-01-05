823f51fa720e65016954b75ec17b5855025dcb5d
LimbersMay <limbermay1@gmail.com>
2024-01-05 17:43:11 -0600

    Add useProductStore hook to fetch and manage product data

--- src/apps/webStoreBackOffice/hooks/useProductStore.ts
+++ src/apps/webStoreBackOffice/hooks/useProductStore.ts
@@ -1,1 +1,43 @@
+import {collection, getDocs} from "firebase/firestore/lite";
+import {selectAuth, useAppDispatch, useAppSelector} from "../store";
+import {onSetActiveProduct, selectProduct, setProducts} from "../store/backOffice";
+import {FirebaseDB} from "../firebase";
 
+
+export const useProductStore = () => {
+
+    const dispatch = useAppDispatch();
+    const { products, activeProduct } = useAppSelector(selectProduct);
+    const { uid } = useAppSelector(selectAuth);
+
+    const setActiveArea = (product: Product | null) => {
+        dispatch(onSetActiveProduct(product));
+    }
+
+    const startLoadingProducts = async () => {
+        const collectionRef = collection(FirebaseDB, `${uid}/webstore/products`);
+        const docs = await getDocs(collectionRef);
+
+        const products: Product[] = [];
+
+        docs.forEach(doc => {
+            products.push({
+                id: doc.id,
+                ...doc.data()
+            } as Product)
+        });
+
+        dispatch(setProducts(products));
+    }
+
+    return {
+        // Properties
+        products,
+        activeProduct,
+
+
+        // Methods
+        startLoadingProducts,
+        setActiveArea
+    }
+}
