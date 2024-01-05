d701b21e44b727be7f0634e9ffc7c9f0de5f291c
LimbersMay <limbermay1@gmail.com>
2024-01-05 17:41:20 -0600

    Add ProductRow component to display product information in a table row

---
+++ src/apps/webStoreBackOffice/backoffice/components/tableRows/ProductRow.tsx
@@ -0,0 +1,27 @@
+export const RoleRow = (product: Product) => {
+
+    const { id, title, description, price, stock, imageURL } = product;
+
+    return (
+        <tr>
+            <td className="px-4 py-2 border">{id}</td>
+            <td className="px-4 py-2 border">{title}</td>
+            <td className="px-4 py-2 border">{description}</td>
+            <td className="px-4 py-2 border">{imageURL}</td>
+            <td className="px-4 py-2 border">{price}</td>
+            <td className="px-4 py-2 border">{stock}</td>
+            <td className="px-4 py-2 border text-left">
+                <button
+                    className="px-4 py-2 mr-2 w-20 text-white bg-green-600 rounded hover:bg-green-700 disabled:bg-green-400"
+                >
+                    Edit
+                </button>
+                <button
+                    className="px-4 py-2 text-white w-20 bg-red-500 rounded hover:bg-red-600 disabled:bg-red-400"
+                >
+                    Delete
+                </button>
+            </td>
+        </tr>
+    )
+}
