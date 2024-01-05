7fa4c312b763a90903463b3a67f67b71d3b2fb45
LimbersMay <limbermay1@gmail.com>
2024-01-05 17:43:22 -0600

    Add productSlice to backOffice store

---
+++ src/apps/webStoreBackOffice/store/backOffice/productSlice.ts
@@ -0,0 +1,51 @@
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {RootState} from "../store.ts";
+
+interface initialState {
+    products: Product[];
+    activeProduct: Product | null;
+}
+
+const initialState: initialState = {
+    products: [],
+    activeProduct: null
+}
+
+export const productsSlice = createSlice({
+    name: 'products',
+    initialState: initialState,
+    reducers: {
+        setProducts: (state, action: PayloadAction<Product[]>) => {
+            state.products = action.payload;
+        },
+        setProduct: (state, action: PayloadAction<Product>) => {
+            state.products.push(action.payload)
+        },
+        onSetActiveProduct: (state, action: PayloadAction<Product | null>) => {
+            state.activeProduct = action.payload;
+        },
+        updateProduct: (state, action: PayloadAction<Partial<Product>>) => {
+            const index = state.products.findIndex(product => product.id === action.payload.id);
+
+            state.products[index] = {
+                ...state.products[index],
+                ...action.payload
+            };
+        },
+        deleteProduct: (state, action: PayloadAction<string>) => {
+            state.products = state.products.filter(product => product.id !== action.payload);
+        }
+    }
+});
+
+
+// Action creators are generated for each case reducer function
+export const {
+    setProducts,
+    setProduct,
+    onSetActiveProduct,
+    updateProduct,
+    deleteProduct
+} = productsSlice.actions;
+
+export const selectProduct = (state: RootState) => state.product;
