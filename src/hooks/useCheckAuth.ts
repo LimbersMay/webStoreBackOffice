4fd81cbd3cd7b16f61fbce24b77c742dd04d6583
LimbersMay <limbermay1@gmail.com>
2024-01-05 15:40:26 -0600

    Refactor useCheckAuth hook to use Redux and Firebase authentication

--- src/hooks/useCheckAuth.ts
+++ src/hooks/useCheckAuth.ts
@@ -1,5 +1,26 @@
-import {authStatusTypes} from "../auth/types";
+import {useEffect} from "react";
+import {useDispatch} from "react-redux";
+import {onAuthStateChanged} from "firebase/auth";
+import {FirebaseAuth} from "../firebase/config";
+import {login, logout, selectAuth, useAppSelector} from "../store";
+
 
 export const useCheckAuth = () => {
-    return authStatusTypes.notAuthenticated;
+
+    const { status } = useAppSelector(selectAuth);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+
+        onAuthStateChanged(FirebaseAuth, async(user) => {
+            if (!user) return dispatch(logout(null));
+
+            const { uid, email, displayName } = user;
+
+            dispatch(login({ uid, email, displayName }));
+        });
+
+    }, []);
+
+    return status
 }
