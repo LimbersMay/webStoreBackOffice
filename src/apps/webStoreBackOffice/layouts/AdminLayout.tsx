0cc4054c0ea0729464fbcce4961ac667582026f4
LimbersMay <limbermay1@gmail.com>
2024-01-05 17:42:15 -0600

    Refactor AdminLayout component

--- src/apps/webStoreBackOffice/layouts/AdminLayout.tsx
+++ src/apps/webStoreBackOffice/layouts/AdminLayout.tsx
@@ -1,62 +1,18 @@
-import {BaseLayout} from "../../../layouts";
-import {ReactElement, useEffect} from "react";
-import {
-    useAreaStore,
-    useMenuStore,
-    useOrderStore,
-    useRoleStore, useTablesStore,
-    useUsersStore,
-    useUserStateStore
-} from "../../../hooks";
+import {ReactElement} from "react";
+
+import {BaseLayout} from "./BaseLayout.tsx";
+
 export const AdminLayout = ({ children }: { children: ReactElement }) => {
     const navLinks = [
         {
-            name: "Users",
-            path: "/users"
+            name: "Productos",
+            path: "/products"
         },
         {
-            name: "Roles",
-            path: "/roles"
-        },
-        {
-            name: "Areas",
-            path: "/areas"
-        },
-        {
-            name: "Tables",
-            path: "/tables"
-        },
-        {
-            name: "Menus",
-            path: "/menus"
-        },
-        {
-            name: "Orders",
-            path: "/orders"
-        },
+            name: "Categorías",
+            path: "/categories"
+        }
     ]
-
-    // start loading the following data:
-    // 1. Users who were created by the current user
-    const { startLoadingUsers } = useUsersStore();
-    const { startLoadingRoles } = useRoleStore();
-    const { startLoadingMenus } = useMenuStore();
-    const { startLoadingUserStates } = useUserStateStore();
-    const { startLoadingAreas } = useAreaStore();
-    const { startLoadingOrders } = useOrderStore();
-    const { startLoadingTables } = useTablesStore();
-
-    useEffect(() => {
-        (async () => {
-            await startLoadingUsers();
-            await startLoadingRoles();
-            await startLoadingUserStates();
-            await startLoadingAreas();
-            await startLoadingMenus();
-            await startLoadingOrders();
-            await startLoadingTables();
-        })()
-    }, []);
 
     return (
         <BaseLayout navLinks={navLinks}>
