20920abd6b6b4f3a8bb83a3aeb5afe4f09a42137
LimbersMay <limbermay1@gmail.com>
2024-01-05 21:47:11 -0600

    Add NiceModal provider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,17 @@ import './index.css'
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./apps/webStoreBackOffice";
+import NiceModal from "@ebay/nice-modal-react";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-      <Provider store={store}>
-          <BrowserRouter>
-              <App />
-          </BrowserRouter>
-      </Provider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Provider store={store}>
+            <NiceModal.Provider>
+
+                <BrowserRouter>
+                    <App/>
+                </BrowserRouter>
+            </NiceModal.Provider>
+        </Provider>
+    </React.StrictMode>,
 )
