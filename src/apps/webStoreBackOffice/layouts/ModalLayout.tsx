f30b06bea3f3304bf9f5eee27f849345eeb70d56
LimbersMay <limbermay1@gmail.com>
2024-01-05 21:50:00 -0600

    Refactor ModalLayout component to use "action" prop instead of "mode" prop

--- src/apps/webStoreBackOffice/layouts/ModalLayout.tsx
+++ src/apps/webStoreBackOffice/layouts/ModalLayout.tsx
@@ -2,36 +2,34 @@ import {ReactElement} from "react";
 
 interface ModalProps {
     children?: ReactElement | ReactElement[];
-    show: boolean;
-    onClose: () => void;
-    mode: 'Create' | 'Update' | 'Select';
+    action: 'Create' | 'Update' | 'Select';
+    onClose?: () => void;
 }
 
-export const ModalLayout = ({children, mode, show, onClose}: ModalProps) => {
+export const ModalLayout = ({ children, action, onClose }: ModalProps) => {
     return (
         <>
             {
-                show && (
-                    <>
-                        <div className="fixed inset-0 flex items-center justify-center">
-                            <div className="fixed inset-0 bg-black opacity-50" onClick={onClose}></div> {/* Black opacity background */}
+                <>
+                    <div className="fixed inset-0 flex items-center justify-center">
+                        <div className="fixed inset-0 bg-black opacity-50" onClick={onClose}></div>
+                        {/* Black opacity background */}
 
-                            <div className="flex">
-                                <div className="bg-black opacity-50" onClick={onClose}></div>
-                                <div className="relative z-50 bg-white p-4 rounded shadow-lg flex flex-col gap-2">
-                                    <h2 className="text-2xl font-bold text-green-800">
-                                        <span className="text-neutral-950 mr-2">{mode}</span>
-                                        <span className="text-green-800">Record</span>
-                                    </h2>
-                                    <hr/>
-                                    {
-                                        children
-                                    }
-                                </div>
+                        <div className="flex">
+                            <div className="bg-black opacity-50" onClick={onClose}></div>
+                            <div className="relative z-50 bg-white p-4 rounded shadow-lg flex flex-col gap-2">
+                                <h2 className="text-2xl font-bold text-green-800">
+                                    <span className="text-neutral-950 mr-2">{action}</span>
+                                    <span className="text-green-800">Registro</span>
+                                </h2>
+                                <hr/>
+                                {
+                                    children
+                                }
                             </div>
                         </div>
-                    </>
-                )
+                    </div>
+                </>
             }
         </>
     );
