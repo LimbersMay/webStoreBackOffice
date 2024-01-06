a984634dcfb031aeecce431c2e9d48217254ac36
LimbersMay <limbermay1@gmail.com>
2024-01-05 21:48:57 -0600

    Refactor Products page layout and translations

--- src/apps/webStoreBackOffice/backoffice/pages/Products.tsx
+++ src/apps/webStoreBackOffice/backoffice/pages/Products.tsx
@@ -1,30 +1,29 @@
-import {AdminLayout} from "../../layouts/AdminLayout.tsx";
-import {useProductStore} from "../../hooks/useProductStore.ts";
+import {AdminLayout} from "../../layouts";
+import {useProductStore} from "../../hooks";
 import {CrudTable, RoleRow} from "../components";
 
 export const Products = () => {
 
     const headers = [
         'ID',
-        'Title',
-        'Description',
-        'ImageURL',
-        'Price',
+        'Título',
+        'Descripción',
+        'URL de la imagen',
+        'Precio',
         'Stock'
     ]
 
-    const { products } = useProductStore();
-
+    const { products, startCreatingProduct } = useProductStore();
 
     return (
         <AdminLayout>
             <div className="p-10">
                 <h1 className="text-4xl font-bold text-neutral-900">
-                    Products
+                    Productos
                 </h1>
 
-                <button className="px-4 py-2 mt-4 text-white bg-green-500 rounded hover:bg-green-600">
-                    Create Role
+                <button className="px-4 py-2 mt-4 text-white bg-green-500 rounded hover:bg-green-600" onClick={startCreatingProduct}>
+                    Crear producto
                 </button>
 
                 <CrudTable headers={headers}>
