ef85e0cff6944a7d17d31cbf125dcfefdfa12896
LimbersMay <limbermay1@gmail.com>
2024-01-06 15:26:37 -0600

    Refactor useStoreApp hook to load store products and categories from Firebase

--- src/apps/ecommerceApp/hooks/useStoreApp.ts
+++ src/apps/ecommerceApp/hooks/useStoreApp.ts
@@ -1,7 +1,62 @@
-import {useAppSelector} from "../../webStoreBackOffice";
-import {selectStore} from "../../webStoreBackOffice/store/ecommerceApp/useStore.ts";
+import {
+    useAppSelector,
+    selectStore,
+    FirebaseDB,
+    useAppDispatch,
+    Category,
+    setStoreProducts, setStoreCategories
+} from "../../webStoreBackOffice";
+import {collection, getDocs} from "firebase/firestore/lite";
+import {Product} from "../../webStoreBackOffice";
 
-export const useStore = () => {
-    const { } = useAppSelector(selectStore);
+export const useStoreApp = () => {
 
+    const { storeProducts, storeCategories } = useAppSelector(selectStore);
+    const dispatch = useAppDispatch();
+
+    const startLoadingStoreProducts = async (storeId: string) => {
+
+        const collectionRef = collection(FirebaseDB, `${storeId}/webstore/products`);
+        const docs = await getDocs(collectionRef);
+
+        const products: Product[] = [];
+
+        docs.forEach(doc => {
+
+            products.push({
+                id: doc.id,
+                ...doc.data()
+            } as Product)
+        });
+
+        dispatch(setStoreProducts(products));
+    }
+
+    const startLoadingStoreCategories = async (storeId: string) => {
+
+            const collectionRef = collection(FirebaseDB, `${storeId}/webstore/categories`);
+            const docs = await getDocs(collectionRef);
+
+            const categories: Category[] = [];
+
+            docs.forEach(doc => {
+
+                categories.push({
+                    id: doc.id,
+                    ...doc.data()
+                } as Category)
+            });
+
+            dispatch(setStoreCategories(categories));
+    }
+
+    return {
+        // Properties
+        storeProducts,
+        storeCategories,
+
+        // Methods
+        startLoadingStoreProducts,
+        startLoadingStoreCategories
+    }
 }
