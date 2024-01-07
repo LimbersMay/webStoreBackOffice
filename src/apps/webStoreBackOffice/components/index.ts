ed6382e647d7a2cec7ebca4825b217c78d9dbd73
LimbersMay <limbermay1@gmail.com>
2024-01-07 17:17:33 -0600

    Add ModalImageInput component

--- src/apps/webStoreBackOffice/components/index.ts
+++ src/apps/webStoreBackOffice/components/index.ts
@@ -1,6 +1,7 @@
 export * from './CrudTable';
 export * from './Field';
 export * from './ModalField';
+export * from './ModalImageInput';
 export * from './ModalSelect';
 export * from './modals';
 export * from './tableRows';
