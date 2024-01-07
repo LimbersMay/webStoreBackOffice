1414b5b9cfd7fefbc3e551f58ccd4a8b8c048bf8
LimbersMay <limbermay1@gmail.com>
2024-01-07 10:55:07 -0600

    Refactor BaseLayout component to include search functionality

--- src/apps/ecommerceApp/layouts/BaseLayout.tsx
+++ src/apps/ecommerceApp/layouts/BaseLayout.tsx
@@ -1,58 +1,72 @@
-import {NavLink} from "react-router-dom";
-import {ReactElement} from "react";
-
-interface NavLink {
-    name: string,
-    path: string
-}
+import {FormEvent, ReactElement, useState} from "react";
+import {NavLink, useNavigate, useParams} from "react-router-dom";
 
 interface BaseLayoutProps {
     children: ReactElement,
-    navLinks: NavLink[]
 }
 
-export const BaseLayout = ({ children, navLinks }: BaseLayoutProps ) => {
+export const BaseLayout = ({children}: BaseLayoutProps) => {
 
-    const isActiveLink = ({ isActive}: { isActive: boolean}) => {
+    const [ productTitleToSearch, setProductTitleToSearch ] = useState<string>("");
+    const navigation = useNavigate();
+
+    const isActiveLink = ({isActive}: { isActive: boolean }) => {
 
         const baseClasses = "rounded-md p-2";
 
         if (isActive) {
-            return `${baseClasses} bg-neutral-800 text-neutral-100`;
+            return `${baseClasses} text-neutral-900 font-bold`;
         }
 
-        return `${baseClasses} hover:bg-neutral-800 hover:text-neutral-100 text-black-800`;
+        return `${baseClasses} text-neutral-500 hover:text-neutral-900`;
     }
+
+    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        navigation(`/store/${storeId}?search=${productTitleToSearch}`);
+    }
+
+    const {storeId} = useParams();
 
     return (
         <div>
-            <header className="flex justify-around bg-white p-4 text-xl">
+            <header className="flex justify-around p-3 text-xl">
 
-                <div className="self-center font-bold">
+                <div className="self-center">
                     <NavLink
-                        to="/"
+                        to={`/store/${storeId}`}
                         className={isActiveLink}
                     >
                         Ecommerce App
                     </NavLink>
                 </div>
 
-                <div className="flex items-center gap-5 font-bold">
-                    {
-                        navLinks.map(({ path, name }) => (
-                            <NavLink
-                                key={path}
-                                to={path}
-                                className={isActiveLink}
-                            >
-                                {name}
-                            </NavLink>
-                        ))
-                    }
-                </div>
+
+                {/* Search */}
+                <form
+                    onSubmit={onSubmit}
+                >
+                    <div className="flex justify-center items-center px-4 sm:px-6 lg:px-8">
+                        <input type="text"
+                               className="h-14 w-96 pr-8 pl-5 rounded focus:shadow focus:outline-none focus:text-neutral-800"
+                               placeholder="Buscar producto..."
+                               value={productTitleToSearch}
+                               onChange={(e) => setProductTitleToSearch(e.target.value)}
+                        />
+
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+                            className="w-6 h-6 relative right-10 text-gray-500 hover:text-gray-700 cursor-pointer"
+                        >
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                        </svg>
+                    </div>
+                </form>
+
             </header>
 
-            { children }
+            {children}
         </div>
     )
 }
