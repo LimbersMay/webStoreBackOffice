7ddf6a79bd2d8bfeb4182e8c60f0e56df1654a1b
LimbersMay <limbermay1@gmail.com>
2024-01-07 14:06:13 -0600

    Add storeSettingsSlice and update actions

--- src/apps/webStoreBackOffice/store/backOffice/storeSettingsSlice.ts
+++ src/apps/webStoreBackOffice/store/backOffice/storeSettingsSlice.ts
@@ -12,23 +12,45 @@ const initialState: initialState = {
     bannerURL: 'https://s3.amazonaws.com/thumbnails.venngage.com/template/19fb13e4-a435-4bae-9bbf-ff1ed161cb31.png',
     logoURL: 'https://icon-library.com/images/products-icon/products-icon-25.jpg',
     phoneNumber: '9991112223',
+    bannerName: '',
+    logoName: '',
 }
 
-export const productsSlice = createSlice({
+export const storeSettingsSlice = createSlice({
     name: 'storeSettings',
     initialState: initialState,
     reducers: {
+        setStoreSettings: (state, payload: PayloadAction<StoreSettings>) => {
+            state.id = payload.payload.id;
+            state.title = payload.payload.title;
+            state.description = payload.payload.description;
+            state.bannerURL = payload.payload.bannerURL;
+            state.logoURL = payload.payload.logoURL;
+            state.phoneNumber = payload.payload.phoneNumber;
+        },
+        updateStoreSettings: (state, payload: PayloadAction<Partial<StoreSettings>>) => {
+            const updatedStoreSettings = {
+                ...state,
+                ...payload.payload
+            };
 
+            state.id = updatedStoreSettings.id;
+            state.title = updatedStoreSettings.title;
+            state.description = updatedStoreSettings.description;
+            state.bannerURL = updatedStoreSettings.bannerURL;
+            state.logoURL = updatedStoreSettings.logoURL;
+            state.phoneNumber = updatedStoreSettings.phoneNumber;
+            state.bannerName = updatedStoreSettings.bannerName;
+            state.logoName = updatedStoreSettings.logoName;
+        },
     }
 });
 
 
 // Action creators are generated for each case reducer function
 export const {
-    setProducts,
-    setProduct,
-    updateProduct,
-    deleteProduct
-} = productsSlice.actions;
+    setStoreSettings,
+    updateStoreSettings
+} = storeSettingsSlice.actions;
 
-export const selectProduct = (state: RootState) => state.StoreSettings;
+export const selectStoreSettings = (state: RootState) => state.storeSettings;
