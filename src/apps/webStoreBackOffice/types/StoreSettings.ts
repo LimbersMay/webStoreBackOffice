14f572349c92fa8170db7a01e0032d2f616057ef
LimbersMay <limbermay1@gmail.com>
2024-01-07 14:08:17 -0600

    Refactor StoreConfiguration to StoreSettings

--- src/apps/webStoreBackOffice/types/StoreSettings.ts
+++ src/apps/webStoreBackOffice/types/StoreSettings.ts
@@ -1,7 +1,10 @@
-export interface StoreConfiguration {
+export interface StoreSettings {
+    id: string;
     title: string;
     description: string;
     bannerURL: string;
+    bannerName: string;
     logoURL: string;
+    logoName: string;
     phoneNumber: string;
 }
