3acf7acfebc46d7902f3430defd0b53cac91241c
LimbersMay <limbermay1@gmail.com>
2024-01-07 10:58:01 -0600

    Update Product interface in Product.ts

--- src/apps/webStoreBackOffice/backoffice/types/Product.ts
+++ src/apps/webStoreBackOffice/backoffice/types/Product.ts
@@ -1,9 +1,11 @@
 export interface Product {
     id: string;
     title: string;
-    description: string;
+    description?: string;
     categoryId: string;
     imageURL: string;
+    imageName: string;
     price: number;
     stock: number;
+    image?: File | null;
 }
