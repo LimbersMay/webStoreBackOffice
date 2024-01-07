373b307329dde63ca3328b29abb4bd80bcd65857
LimbersMay <limbermay1@gmail.com>
2024-01-07 17:16:14 -0600

    Refactor useStoreApp hook to include loading store settings

--- src/apps/ecommerceApp/hooks/useStoreApp.ts
+++ src/apps/ecommerceApp/hooks/useStoreApp.ts
@@ -4,14 +4,14 @@ import {
     FirebaseDB,
     useAppDispatch,
     Category,
-    setStoreProducts, setStoreCategories
+    setStoreProducts, setStoreCategories, StoreSettings, setStoreConfiguration
 } from "../../webStoreBackOffice";
 import {collection, getDocs} from "firebase/firestore/lite";
 import {Product} from "../../webStoreBackOffice";
 
 export const useStoreApp = () => {
 
-    const { storeProducts, storeCategories } = useAppSelector(selectStore);
+    const { storeProducts, storeCategories, storeConfiguration } = useAppSelector(selectStore);
     const dispatch = useAppDispatch();
 
     const startLoadingStoreProducts = async (storeId: string) => {
@@ -50,13 +50,27 @@ export const useStoreApp = () => {
             dispatch(setStoreCategories(categories));
     }
 
+    const startLoadingStoreSettings = async (userId: string) => {
+        const collectionRef = collection(FirebaseDB, `${userId}/webstore/store`);
+        const doc = await getDocs(collectionRef);
+
+        const storeSettings = {
+            id: doc.docs[0].id,
+            ...doc.docs[0].data()
+        } as StoreSettings;
+
+        dispatch(setStoreConfiguration(storeSettings));
+    }
+
     return {
         // Properties
         storeProducts,
         storeCategories,
+        ...storeConfiguration,
 
         // Methods
         startLoadingStoreProducts,
-        startLoadingStoreCategories
+        startLoadingStoreCategories,
+        startLoadingStoreSettings
     }
 }
