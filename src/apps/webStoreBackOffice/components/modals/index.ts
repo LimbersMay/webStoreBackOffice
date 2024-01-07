f6f06bd812fb2f81b3d3bbdbda7c8b1ad05c4d38
LimbersMay <limbermay1@gmail.com>
2024-01-07 14:08:21 -0600

    Add new modals for category and store settings

--- src/apps/webStoreBackOffice/components/modals/index.ts
+++ src/apps/webStoreBackOffice/components/modals/index.ts
@@ -1,1 +1,3 @@
-export { default as CreateProductModal } from './ProductModal.tsx';
+export { default as CategoryModal } from './CategoryModal';
+export { default as ProductModal } from './ProductModal';
+export { default as StoreSettingsModal } from './StoreSettingsModal';
