ce8f5578af42860c88c59363b67bc64b02275103
LimbersMay <limbermay1@gmail.com>
2024-01-07 10:57:14 -0600

    Refactor ProductRow component styling

--- src/apps/webStoreBackOffice/backoffice/components/tableRows/ProductRow.tsx
+++ src/apps/webStoreBackOffice/backoffice/components/tableRows/ProductRow.tsx
@@ -15,22 +15,22 @@ export const ProductRow = (product: Product) => {
 
     return (
         <tr>
-            <td className="px-4 py-2 border">{id}</td>
-            <td className="px-4 py-2 border">{title}</td>
-            <td className="px-4 py-2 border">{description}</td>
-            <td className="px-4 py-2 border">{categoryTitle}</td>
-            <td className="px-4 py-2 border">{imageURL}</td>
-            <td className="px-4 py-2 border">{price}</td>
-            <td className="px-4 py-2 border">{stock}</td>
-            <td className="px-4 py-2 border text-left">
+            <td className="border px-4 py-2">{id}</td>
+            <td className="border px-4 py-2">{title}</td>
+            <td className="border px-4 py-2">{description}</td>
+            <td className="border px-4 py-2">{categoryTitle}</td>
+            <td className="border px-4 py-2 overflow-hidden overflow-ellipsis text-nowrap max-h-10 max-w-10">{imageURL}</td>
+            <td className="border px-4 py-2">{price}</td>
+            <td className={`px-4 py-2 border ${ stock > 0 ? "bg-green-500 text-white" : "bg-red-500 text-white"}`}>{stock}</td>
+            <td className="border px-4 py-2 text-left">
                 <button
-                    className="px-4 py-2 mr-2 w-20 text-white bg-green-600 rounded hover:bg-green-700 disabled:bg-green-400"
+                    className="mr-2 w-20 rounded bg-green-600 px-4 py-2 text-white hover:bg-green-700 disabled:bg-green-400"
                     onClick={handleEdit}
                 >
                     Edit
                 </button>
                 <button
-                    className="px-4 py-2 text-white w-20 bg-red-500 rounded hover:bg-red-600 disabled:bg-red-400"
+                    className="w-20 rounded bg-red-500 px-4 py-2 text-white hover:bg-red-600 disabled:bg-red-400"
                     onClick={() => startDeletingProduct(id)}
                 >
                     Delete
