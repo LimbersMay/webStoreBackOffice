dcacedf502a91becebd032763d1979687c3252f4
LimbersMay <limbermay1@gmail.com>
2024-01-07 11:08:22 -0600

    Add StoreConfig page

---
+++ src/apps/webStoreBackOffice/pages/StoreConfig.tsx
@@ -0,0 +1,9 @@
+import {AdminLayout} from "../layouts";
+
+export const StoreConfig = () => {
+    return (
+        <AdminLayout>
+            <h1>StoreConfig</h1>
+        </AdminLayout>
+    )
+}
