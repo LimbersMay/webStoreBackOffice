982348072ecf66b2cb3af551516685dd59b58025
LimbersMay <limbermay1@gmail.com>
2024-01-07 14:07:52 -0600

    Add StoreConfig table to StoreConfig page

--- src/apps/webStoreBackOffice/pages/StoreConfig.tsx
+++ src/apps/webStoreBackOffice/pages/StoreConfig.tsx
@@ -1,9 +1,23 @@
 import {AdminLayout} from "../layouts";
+import {CrudTable, StoreConfigRow} from "../components";
 
 export const StoreConfig = () => {
+
+    const headers = ["ID", "Título", "Descripción", "Logo URL", "Banner URL", "Teléfono", "Acciones"];
+
     return (
         <AdminLayout>
-            <h1>StoreConfig</h1>
+                <div
+                    className="py-20 px-20 flex flex-col gap-5"
+                >
+                    <h1 className="text-4xl text-neutral-800 font-bold">
+                        Configuración de la tienda
+                    </h1>
+
+                    <CrudTable headers={headers}>
+                        <StoreConfigRow />
+                    </CrudTable>
+                </div>
         </AdminLayout>
     )
 }
