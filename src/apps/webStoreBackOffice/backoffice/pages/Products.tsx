a8b2be7ac03fb09297be22f5ec66bade3090365d
LimbersMay <limbermay1@gmail.com>
2024-01-07 10:57:57 -0600

    Add pagination functionality to Products page

--- src/apps/webStoreBackOffice/backoffice/pages/Products.tsx
+++ src/apps/webStoreBackOffice/backoffice/pages/Products.tsx
@@ -1,6 +1,7 @@
 import {AdminLayout} from "../../layouts";
 import {useProductStore} from "../../hooks";
 import {CrudTable, ProductRow} from "../components";
+import {useState} from "react";
 
 export const Products = () => {
 
@@ -16,6 +17,15 @@ export const Products = () => {
 
     const { products, startCreatingProduct } = useProductStore();
 
+    const totalPages = Math.ceil(products.length / 5);
+    const end = 8;
+
+    const [range, setRange] = useState({
+        start: 0,
+        end,
+        current: 1
+    });
+
     return (
         <AdminLayout>
             <div className="p-10">
@@ -23,17 +33,45 @@ export const Products = () => {
                     Productos
                 </h1>
 
-                <button className="px-4 py-2 mt-4 text-white bg-green-500 rounded hover:bg-green-600" onClick={startCreatingProduct}>
+                <button className="mt-4 rounded bg-green-500 px-4 py-2 text-white hover:bg-green-600" onClick={startCreatingProduct}>
                     Crear Producto
                 </button>
 
                 <CrudTable headers={headers}>
                     {
-                        products.map((product) => (
+                        products.slice(range.start, range.end).map((product) => (
                             <ProductRow key={product.id} {...product} />
                         ))
                     }
                 </CrudTable>
+
+                <div className="mt-4 flex justify-center">
+                    <button className="rounded bg-gray-500 px-4 py-2 text-white hover:bg-gray-600 disabled:bg-gray-400" onClick={() => setRange({
+                        start: range.start - end,
+                        end: range.end - end,
+                        current: range.current - 1
+                    })}
+                            disabled={range.current === 1}
+                    >
+                        Anterior
+                    </button>
+
+                    <span className="px-4 py-2 font-bold text-gray-800">
+                        {range.current} de {totalPages}
+                    </span>
+
+                    <button className="ml-4 rounded bg-green-500 px-4 py-2 text-white hover:bg-green-600 disabled:bg-green-400" onClick={() => setRange({
+                        start: range.start + end,
+                        end: range.end + end,
+                        current: range.current + 1
+                    })}
+                            disabled={range.current === totalPages}
+                    >
+                        Siguiente
+                    </button>
+
+                </div>
+
             </div>
         </AdminLayout>
     )
