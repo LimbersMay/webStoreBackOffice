5d5a346c3a837ad4163cfc5a6cdb95479b4119df
LimbersMay <limbermay1@gmail.com>
2024-01-07 10:57:07 -0600

    Refactor ProductModal component

--- src/apps/webStoreBackOffice/backoffice/components/modals/ProductModal.tsx
+++ src/apps/webStoreBackOffice/backoffice/components/modals/ProductModal.tsx
@@ -32,21 +32,17 @@ const ProductModal = NiceModal.create(({action, product}: ProductModalProps) => 
                 validationSchema={Yup.object({
                     title: Yup.string()
                         .required('Required'),
-                    description: Yup.string()
-                        .required('Required'),
-                    imageURL: Yup.string()
-                        .required('Required'),
                     price: Yup.number()
-                        .moreThan(0, 'Must be greater than 0')
                         .required('Required'),
                     stock: Yup.number()
-                        .moreThan(0, 'Must be greater than 0')
                         .required('Required'),
 
                 })}
             >
                 {
-                    ({}) => (
+                    ({
+                        setFieldValue
+                     }) => (
                         <Form className="flex flex-col gap-2">
 
                             <ModalField name="title" type="text" fieldName={"Título"}/>
@@ -57,6 +53,7 @@ const ProductModal = NiceModal.create(({action, product}: ProductModalProps) => 
 
                             <ModalSelect
                                 name="categoryId"
+                                fieldName={"Categoría"}
                                 options={
                                 categories.map((category) => ({
                                     value: category.id,
@@ -64,8 +61,22 @@ const ProductModal = NiceModal.create(({action, product}: ProductModalProps) => 
                                 }))}
                             />
 
-                            <ModalField name="imageURL" type="text" fieldName={"URL de la imagen"}/>
-                            <ErrorMessage name="imageURL" component="div" className="font-bold text-red-500"/>
+                            <div className="mb-3">
+                                <label
+                                    htmlFor={"formFile"}
+                                    className="mb-2 inline-block font-bold text-green-800"
+                                >Imagen</label
+                                >
+                                <input
+                                    className="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-600 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-400 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
+                                    type="file"
+                                    id="formFile"
+                                    name="image"
+                                    onChange={async (event) => {
+                                        await setFieldValue("image", event.currentTarget.files?.[0]);
+                                    }}
+                                />
+                            </div>
 
                             <ModalField name="price" type="number" fieldName={"Precio"}/>
                             <ErrorMessage name="price" component="div" className="font-bold text-red-500"/>
@@ -75,7 +86,7 @@ const ProductModal = NiceModal.create(({action, product}: ProductModalProps) => 
 
                             <button
                                 type="submit"
-                                className="px-4 py-2 mt-4 text-white bg-green-500 rounded hover:bg-green-600"
+                                className="mt-4 rounded bg-green-500 px-4 py-2 text-white hover:bg-green-600"
                             >
                                 {
                                     action
