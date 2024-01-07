f2f76165336597a7d7e8b507751c9d1fbe35894f
LimbersMay <limbermay1@gmail.com>
2024-01-07 11:08:16 -0600

    Refactor ModalLayout component styles

--- src/apps/webStoreBackOffice/layouts/ModalLayout.tsx
+++ src/apps/webStoreBackOffice/layouts/ModalLayout.tsx
@@ -17,9 +17,9 @@ export const ModalLayout = ({ children, action, onClose }: ModalProps) => {
 
                         <div className="flex">
                             <div className="bg-black opacity-50" onClick={onClose}></div>
-                            <div className="relative z-50 bg-white p-4 rounded shadow-lg flex flex-col gap-2">
+                            <div className="relative z-50 flex flex-col gap-2 rounded bg-white p-4 shadow-lg">
                                 <h2 className="text-2xl font-bold text-green-800">
-                                    <span className="text-neutral-950 mr-2">{action}</span>
+                                    <span className="mr-2 text-neutral-950">{action}</span>
                                     <span className="text-green-800">Registro</span>
                                 </h2>
                                 <hr/>
